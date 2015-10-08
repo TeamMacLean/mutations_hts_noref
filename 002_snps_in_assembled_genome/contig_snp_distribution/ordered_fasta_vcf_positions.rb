ff66b322d68104c15f9c62fc857e176e2e8adc05
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-10-08 16:51:33 +0100

    modified script to produce snp density per selected chunks of basepairs

--- 002_snps_in_assembled_genome/contig_snp_distribution/ordered_fasta_vcf_positions.rb
+++ 002_snps_in_assembled_genome/contig_snp_distribution/ordered_fasta_vcf_positions.rb
@@ -18,24 +18,28 @@ file.each do |seq|
 	assembly_len += seq.length.to_i
 end
 
+def rename_chr(chr)
+	if chr =~ /^Chr\d/
+		chr.gsub!(/^Chr/, '')
+	elsif chr =~ /^ChrM/
+		chr.gsub!(/^ChrM/, 'mitochondria')
+	elsif chr =~ /^ChrC/
+		chr.gsub!(/^ChrC/, 'chloroplast')
+	end
+	chr
+end
 
 ### Read sequence fasta file and store sequences in a hash
-contigs = Hash.new {|h,k| h[k] = {} }
-File.open(ARGV[1], 'r').each do |line|
+contigs = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+infile = ARGV[1]
+File.open(infile, 'r').each do |line|
 	next if line =~ /^#/
 	v = Bio::DB::Vcf.new(line)
-	if v.info["AF"].to_f == 1.0
-		if contigs[v.chrom].has_key?(:hm)
-			contigs[v.chrom][:hm] += 1
-		else
-			contigs[v.chrom][:hm] = 1
-		end
-	elsif v.info["AF"].to_f == 0.5
-		if contigs[v.chrom].has_key?(:ht)
-			contigs[v.chrom][:ht] += 1
-		else
-			contigs[v.chrom][:ht] = 1
-		end
+	v.chrom = rename_chr(v.chrom)
+	if v.info["HOM"].to_i == 1
+		contigs[v.chrom][:hm][v.pos.to_i] = 1
+	elsif v.info["HET"].to_i == 1
+		contigs[v.chrom][:ht][v.pos.to_i] = 1
 	end
 end
 
@@ -46,11 +50,63 @@ sequences.each_key do |key|
 	ht = 0
 	if contigs.has_key?(key)
 		if contigs[key].has_key?(:hm)
-			hm = contigs[key][:hm]
+			hm = contigs[key][:hm].length
 		end
 		if contigs[key].has_key?(:ht)
-			ht = contigs[key][:ht]
+			ht = contigs[key][:ht].length
 		end
 	end
 	puts "#{key}\t#{info[0]}\t#{info[1]}\t#{hm}\t#{ht}"
+end
+
+# New file is opened to write
+def open_new_file_to_write(input, number)
+	outfile = File.new("per-#{number}bp-#{input}.txt", "w")
+	outfile.puts "position\tnumhm\tnumht"
+	outfile
+end
+
+def enumerate_snps(outhash, step, vartype)
+	if outhash[step].has_key?(vartype)
+		outhash[step][vartype] += 1
+	else
+		outhash[step][vartype] = 1
+	end
+	outhash
+end
+
+def pool_variants_per_step(inhash, assembly, slice, step, outhash, vartype)
+	inhash[vartype].keys.sort.each do | pos |
+		pos += assembly.to_i
+		outhash[step][vartype] = 0
+		if pos <= step
+			outhash = enumerate_snps(outhash, step, vartype)
+		else
+			num_loop = ((pos-step)/slice.to_f).ceil
+			for i in 1..num_loop do
+				step += slice
+				if outhash[step].key?(vartype) == FALSE
+					outhash[step][vartype] = 0
+				end
+			end
+			outhash = enumerate_snps(outhash, step, vartype)
+		end
+	end
+	[outhash, step]
+end
+
+breaks = [10000, 50000, 100000, 500000, 1000000]
+breaks.each do | step |
+	slice = step
+	outfile = open_new_file_to_write(infile, step)
+	distribute = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+	contigs.keys.sort.each do | chrom |
+		info = sequences[chrom].split(/_/)
+		distribute, step = pool_variants_per_step(contigs[chrom], info[0], slice, step, distribute, :hm)
+		#distribute, step = pool_variants_per_step(contigs[chrom], info[0], slice, step, distribute, :ht)
+		#step = step
+	end
+	distribute.each_key do | key |
+		outfile.puts "#{key}\t#{distribute[key][:hm]}\t#{distribute[key][:ht]}"
+	end
 end
