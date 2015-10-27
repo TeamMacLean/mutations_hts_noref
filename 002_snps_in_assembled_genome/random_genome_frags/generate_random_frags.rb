db0dca9c3c73839bb0131749f89564b9d5dd2ad5
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-10-27 16:52:10 +0000

    script modified to generate both ordered and shuffled sequences resulted from all chromosomes

--- 002_snps_in_assembled_genome/random_genome_frags/generate_random_frags.rb
+++ 002_snps_in_assembled_genome/random_genome_frags/generate_random_frags.rb
@@ -1,13 +1,15 @@
 require 'simple-random'
 require 'bio'
 
+# mean, std deviation and sample size to generate random numbers
 mean = ARGV[0].to_i
 sample = ARGV[1].to_i
 sd = ARGV[2].to_i
 
+# generate an array of random numbers using mean and sample number provided
+# exponential distribution is used
 @random = SimpleRandom.new
 @random.set_seed
-
 number_array = []
 (1..sample).each do
 	number = @random.exponential(mean).to_i
@@ -16,28 +18,42 @@ number_array = []
 	end
 end
 
-seq_string = []
-n = 0
+# a hash of chromosome sequences
+chrseq = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
 Bio::FastaFormat.open(ARGV[3]).each do |i|
-	seq_string[n] = i.seq.to_s
-	n += 1
+	chrseq[i.entry_id] = i.seq.to_s
+	warn "#{i.entry_id}\n"
 end
 
-frags = []
-seq_string.each do | seq |
+# chromosme sequences are fragemened to the sizes in random number array
+# and saved to a hash
+frags = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+name_index = 1
+chrseq.keys.sort.each do | id |
+	seq = chrseq[id]
 	while seq.length > 500
 		if seq.length < 1000
-			frags << seq.slice!(0..-1)
+			frags[name_index] = seq.slice!(0..-1)
 		else
 			index = number_array.shift.to_i
-			frags << seq.slice!(0...index)
+			frags[name_index] = seq.slice!(0...index)
 		end
+		name_index += 1
 	end
 end
 
-name_index = 1
-frags.each do | fragment |
-	seqout = Bio::Sequence::NA.new(fragment)
-	puts seqout.to_fasta("seq_id_#{name_index}", 80)
-	name_index += 1
+# write fasta frag hash to a file
+# array of ids provided either sorted or shuffled
+def write_fasta(hash, array, filename)
+	outfile = File.open(filename, 'w')
+	array.each do | element |
+		seqout = Bio::Sequence::NA.new(hash[element])
+		outfile.puts seqout.to_fasta("seq_id_#{element}", 80)
+	end
 end
+
+write_fasta(frags, frags.keys.sort, 'frags_ordered.fasta')
+
+shuffled = frags.keys.shuffle(random: Random.new_seed)
+write_fasta(frags, shuffled, 'frags_shuffled.fasta')
+
