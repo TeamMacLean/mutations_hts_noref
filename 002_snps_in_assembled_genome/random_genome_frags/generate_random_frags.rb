2459a58214ce3feb7acccd0a555582c88114678b
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-10-29 11:34:17 +0000

    generate random fragments over many iterations and writes each to a folder

--- 002_snps_in_assembled_genome/random_genome_frags/generate_random_frags.rb
+++ 002_snps_in_assembled_genome/random_genome_frags/generate_random_frags.rb
@@ -1,31 +1,44 @@
 require 'simple-random'
 require 'bio'
+require 'fileutils'
 
 # mean, std deviation and sample size to generate random numbers
-mean = ARGV[0].to_i   #
-sample = ARGV[1].to_i #
-sd = ARGV[2].to_i     #
+# add additonal 10% to sample number to be able cover the whole genome length
+mean = ARGV[0].to_i   # 11885 bp is mean
+sample = ARGV[1].to_i # 9600 + 500 random numbers (contig number)
+sd = ARGV[2].to_i     # 30160 bp is std. deviation
 
 # a hash of chromosome sequences
+genome_length = 0
 chrseq = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
-Bio::FastaFormat.open(ARGV[3]).each do |i|
-	chrseq[i.entry_id] = i.seq.to_s
-	warn "#{i.entry_id}\n"
+Bio::FastaFormat.open(ARGV[3]).each do |inseq|
+	chrseq[inseq.entry_id] = inseq.seq.to_s
+	genome_length += inseq.length
+	warn "#{inseq.entry_id}\n"
 end
 
 iterations = ARGV[4].to_i
+@random = SimpleRandom.new
+FileUtils.mkdir_p "outseq_lengths"
 
-(1..iterations).each do
+for iteration in 1..iterations
 	# generate an array of random numbers using mean and sample number provided
 	# exponential distribution is used
-	@random = SimpleRandom.new
 	@random.set_seed
 	number_array = []
-	(1..sample).each do
+	i = 0
+	while i < sample
 		number = @random.exponential(mean).to_i
 		if number >= 500
 			number_array << number
+			i += 1
 		end
+	end
+
+	# adding a break to the loop if the random numbers do not cover the genome
+	if number_array.reduce(:+) < genome_length
+		warn "Increase sample number to cover the genome length\n"
+		break
 	end
 
 	# chromosme sequences are fragemened to the sizes in random number array
@@ -55,9 +68,19 @@ iterations = ARGV[4].to_i
 		end
 	end
 
-	write_fasta(frags, frags.keys.sort, 'frags_ordered.fasta')
+	newname = "genome_" + iteration.to_s
+	FileUtils.mkdir_p "#{newname}"
+
+	write_fasta(frags, frags.keys.sort, "#{newname}/frags_ordered.fasta")
 
 	shuffled = frags.keys.shuffle(random: Random.new_seed)
-	write_fasta(frags, shuffled, 'frags_shuffled.fasta')
+	write_fasta(frags, shuffled, "#{newname}/frags_shuffled.fasta")
+
+	output = File.open("outseq_lengths/#{newname}_lengths.txt", 'w')
+	Bio::FastaFormat.open("#{newname}/frags_shuffled.fasta").each do |i|
+    	output.puts "#{i.entry_id}\t#{i.length}"
+	end
+
+	warn "sequences fragment iteration:\t#{iteration}\n"
 
 end
