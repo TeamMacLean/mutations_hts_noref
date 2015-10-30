c1b96165ee06498c3704c687d32a61900be23b85
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-10-30 17:28:24 +0000

    ratios calculated for each row by addition of provided number ...  and no. of homozygous or heterozygous variant information is not altered

--- lib/zero_ratio_adj.R
+++ lib/zero_ratio_adj.R
@@ -1,17 +1,14 @@
-## funcation to replace zeros with selected quantity and calculate ratio 
-## for contigs/scaffolds with zero variants, replacing denominator (heterozygote) value
-## with one to give zero value for such contigs
+## funcation to adjust zeros with selected quantity to calculate ratio
+## for contigs/scaffolds with zero variants ratio value is set to zero
 re_zero_ratio = function(df, number){
+  df$ratio <- df$numhm/df$numht
   for (i in 1:nrow(df)) {
     if (df$numhm[i] == 0 && df$numht[i] == 0) {
-      df$numht[i] = 1
-      #df$numhm[i] = number
+      df$ratio = 0
     }
     else {
-      df$numhm[i] = number + df$numhm[i]
-      df$numht[i] = number + df$numht[i]
+      df$ratio[i] = (number + df$numhm[i])/(number + df$numht[i])
     }
   }
-  df$ratio <- df$numhm/df$numht
   df
 }
