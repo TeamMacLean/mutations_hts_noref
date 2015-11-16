0ed3dcecaa2d6167dbeabca3a253a2702b7fae45
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-11-16 14:34:01 +0000

    removed predicted models from polyfit of the data

--- 002_snps_in_assembled_genome/random_genome_frags/sup2_data_random_fragments/all_data_analysis_scripts/all_data_analysis_individual_rep.R
+++ 002_snps_in_assembled_genome/random_genome_frags/sup2_data_random_fragments/all_data_analysis_scripts/all_data_analysis_individual_rep.R
@@ -22,46 +22,22 @@ for (i in 1:10) {
   selected <- re_zero_ratio(selected, 0.5)
   selected$adjratio <- selected$ratio/selected$length
 
-  # fit polynomial to combined data
-  lmfit2 <- lm(selected$ratio ~ poly(selected$position, 2, raw=TRUE))
-  lmfit3 <- lm(selected$ratio ~ poly(selected$position, 3, raw=TRUE))
-  lmfit4 <- lm(selected$ratio ~ poly(selected$position, 4, raw=TRUE))
-  lmfit5 <- lm(selected$ratio ~ poly(selected$position, 5, raw=TRUE))
-  lmfit6 <- lm(selected$ratio ~ poly(selected$position, 6, raw=TRUE))
-
   iteration = gsub("_varinfo.txt$", "", filelist[i])
   fragid = as.character(frags[which(frags$iterations == iteration),]$fragment)
   selected = within(selected, {
     color=ifelse(fragment==fragid, "red", "black")
     width=ifelse(fragment==fragid, 30, 0.5)})
 
-  # print models to pdf
+  # print non-normalized ratio barplots
   filename1 = paste("barplots_selected/ratio_", filelist[i], "_barplot.pdf", sep='')
   pdf(filename1,width=6,height=4)
   barplot(selected$ratio, width=selected$width, col=selected$color, border=selected$color)
-  lines(predict(lmfit2, data.frame(x=selected$position)), col="red")
-  lines(predict(lmfit3, data.frame(x=selected$position)), col="blue")
-  lines(predict(lmfit4, data.frame(x=selected$position)), col="green")
-  lines(predict(lmfit5, data.frame(x=selected$position)), col="orange")
-  lines(predict(lmfit6, data.frame(x=selected$position)), col="grey")
   dev.off()
 
-  # fit polynomial to combined data
-  lmfit2 <- lm(selected$adjratio ~ poly(selected$position, 2, raw=TRUE))
-  lmfit3 <- lm(selected$adjratio ~ poly(selected$position, 3, raw=TRUE))
-  lmfit4 <- lm(selected$adjratio ~ poly(selected$position, 4, raw=TRUE))
-  lmfit5 <- lm(selected$adjratio ~ poly(selected$position, 5, raw=TRUE))
-  lmfit6 <- lm(selected$adjratio ~ poly(selected$position, 6, raw=TRUE))
-
-  # print models to pdf
+  # print length-normalized ratio barplots
   filename2 = paste("barplots_selected/adjratio_", filelist[i], "_barplot.pdf", sep='')
   pdf(filename2,width=6,height=4)
   barplot(selected$adjratio, width=selected$width, col=selected$color, border=selected$color)
-  lines(predict(lmfit2, data.frame(x=selected$position)), col="red")
-  lines(predict(lmfit3, data.frame(x=selected$position)), col="blue")
-  lines(predict(lmfit4, data.frame(x=selected$position)), col="green")
-  lines(predict(lmfit5, data.frame(x=selected$position)), col="orange")
-  lines(predict(lmfit6, data.frame(x=selected$position)), col="grey")
   dev.off()
 
 
