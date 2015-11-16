3bcbb3ccd9c43579250148af2c960f66701e4053
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-11-16 11:59:00 +0000

    r script to pool data in to bins of 500kb, either by chunks or by sliding window of each fragment

---
+++ 002_snps_in_assembled_genome/random_genome_frags/sup2_data_random_fragments/all_data_analysis_scripts/all_data_analysis_frag2bins.R
@@ -0,0 +1,94 @@
+# load libraries and data
+library(ggplot2)
+library(quantchem)
+source('~/git-hub-repos/shyamrallapalli/mutations_hts_noref/lib/zero_ratio_adj.R')
+
+
+frags <- read.delim(file="fragments_with_mut_ids.txt", header = TRUE)
+colnames(frags) <- c("iterations", "fragment")
+
+dir = getwd()
+dir = paste(dir, "vars_infrags", sep='/')
+dir.create("barplots_frag2bins")
+## read all files in a folder and store to a dataframe
+filelist = list.files(dir, "^genome_")
+#for (i in 1:length(filelist)) {
+for (i in 1:10) {
+  filename = paste(dir, filelist[i], sep='/')
+  newdf <- read.delim(file=filename, header = TRUE)
+  newdf$vars = newdf$numhm + newdf$numht
+  selected = subset(newdf, vars > 0)
+
+  # fragments pooled to a near 500kb chunks and plotted
+  rollingdf1 = data.frame(length=numeric(),
+    numhm=numeric(),
+    numht=numeric(),
+    stringsAsFactors=FALSE)
+
+  dflength = nrow(selected)
+  lenbin = 500000
+  len = 0
+  hm = 0
+  ht = 0
+  x = 1
+  for (i in 1:dflength){
+    len = len + selected$length[i]
+    hm = hm + selected$numhm[i]
+    ht = ht + selected$numht[i]
+    if (len >= lenbin) {
+      rollingdf1[x, ] = c(len, hm, ht)
+      len = 0
+      hm = 0
+      ht = 0
+      x = x + 1
+    }
+    else if (i == dflength){
+      rollingdf1[x, ] = c(len, hm, ht)
+    }
+  }
+  rollingdf1 <- re_zero_ratio(rollingdf1, 0.5)
+
+  # print models to pdf
+  filename1 = paste("barplots_frag2bins/chunks_", filelist[i], ".pdf", sep='')
+  pdf(filename1,width=6,height=4)
+  barplot(rollingdf1$ratio)
+  dev.off()
+
+# fragments pooled to a near 500kb sliding chunks and plotted
+    rollingdf2 = data.frame(length=numeric(),
+    numhm=numeric(),
+    numht=numeric(),
+    stringsAsFactors=FALSE)
+
+  len = 0
+  hm = 0
+  ht = 0
+  x = 1
+  i = 1
+  while (i <= dflength){
+    len = len + selected$length[i]
+    hm = hm + selected$numhm[i]
+    ht = ht + selected$numht[i]
+    i = i + 1
+    if (len >= lenbin) {
+      rollingdf2[x, ] = c(len, hm, ht)
+      len = 0
+      hm = 0
+      ht = 0
+      x = x + 1
+      i = x
+    }
+    else if (i == dflength){
+      rollingdf2[x, ] = c(len, hm, ht)
+    }
+
+  }
+  rollingdf2 <- re_zero_ratio(rollingdf2, 0.5)
+
+  # print models to pdf
+  filename2 = paste("barplots_frag2bins/slidingwindow_", filelist[i], ".pdf", sep='')
+  pdf(filename2,width=6,height=4)
+  barplot(rollingdf2$ratio)
+  dev.off()
+
+}
