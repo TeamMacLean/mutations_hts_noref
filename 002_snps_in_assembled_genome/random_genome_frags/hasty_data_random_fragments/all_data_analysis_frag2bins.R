cd0fac407659acfc9b2c91d9d77e52e1492e0281
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2015-11-19 11:12:57 +0000

    sliding windows analysis of hasty fragment data for selected iterations

---
+++ 002_snps_in_assembled_genome/random_genome_frags/hasty_data_random_fragments/all_data_analysis_frag2bins.R
@@ -0,0 +1,89 @@
+# load libraries and data
+library(ggplot2)
+library(quantchem)
+source('~/git-hub-repos/shyamrallapalli/mutations_hts_noref/lib/zero_ratio_adj.R')
+
+
+frags <- read.delim(file="fragment_with_mut_iterations.txt", header = TRUE)
+colnames(frags) <- c("iterations", "fragment")
+
+dir = getwd()
+dir = paste(dir, "vars_infrags", sep='/')
+dir.create("barplots_frag2bins")
+## read all files in a folder and store to a dataframe
+filelist = list.files(dir, "^genome_")
+#for (i in 1:length(filelist)) {
+for (i in 1:10) {
+  filename = paste(dir, filelist[i], sep='/')
+  newdf <- read.delim(file=filename, header = TRUE)
+  newdf$vars = newdf$numhm + newdf$numht
+  selected = subset(newdf, vars > 0)
+
+  # fragments pooled to a near 500kb chunks and plotted
+  rollingdf1 = data.frame(length=numeric(),
+    numhm=numeric(),
+    numht=numeric(),
+    stringsAsFactors=FALSE)
+
+  dflength = nrow(selected)
+  lenbin = 50000
+  len = 0
+  hm = 0
+  ht = 0
+  x = 1
+  for (j in 1:dflength){
+    len = len + selected$length[j]
+    hm = hm + selected$numhm[j]
+    ht = ht + selected$numht[j]
+    if (len >= lenbin) {
+      rollingdf1[x, ] = c(len, hm, ht)
+      len = 0
+      hm = 0
+      ht = 0
+      x = x + 1
+    }
+    else if (j == dflength){
+      rollingdf1[x, ] = c(len, hm, ht)
+    }
+  }
+  rollingdf1 <- re_zero_ratio(rollingdf1, 0.5)
+
+  # fragments pooled to a near 500kb sliding chunks and plotted
+  rollingdf2 = data.frame(length=numeric(),
+    numhm=numeric(),
+    numht=numeric(),
+    stringsAsFactors=FALSE)
+
+  len = 0
+  hm = 0
+  ht = 0
+  x = 1
+  j = 1
+  while (j <= dflength){
+    len = len + selected$length[j]
+    hm = hm + selected$numhm[j]
+    ht = ht + selected$numht[j]
+    j = j + 1
+    if (len >= lenbin) {
+      rollingdf2[x, ] = c(len, hm, ht)
+      len = 0
+      hm = 0
+      ht = 0
+      x = x + 1
+      j = x
+    }
+    else if (j == dflength){
+      rollingdf2[x, ] = c(len, hm, ht)
+    }
+  }
+  rollingdf2 <- re_zero_ratio(rollingdf2, 0.5)
+
+  # print models to pdf
+  filename1 = paste("barplots_frag2bins/chunks_", filelist[i], ".pdf", sep='')
+  pdf(filename1,width=6,height=6)
+  par(mfrow=c(2,1), mar=c(1,2,1,0.5))
+  barplot(rollingdf1$ratio, main="chunks_50KB")
+  barplot(rollingdf2$ratio, main="slidingwindow_50KB")
+  dev.off()
+
+}
