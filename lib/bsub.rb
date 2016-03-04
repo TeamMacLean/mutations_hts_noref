bc70c5d2cc55f667a0809092eeae6dc12bd2288d
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2016-03-04 15:01:08 +0000

    sbatch submits from command line instead from a temp script

--- lib/bsub.rb
+++ lib/bsub.rb
@@ -2,17 +2,6 @@
 # encoding: utf-8
 #
 
-# make unique file name based on username and time
-# taken from http://stackoverflow.com/a/14508272
-def file_timestamp(file)
-  dir  = File.dirname(file)
-  base = File.basename(file, ".*")
-  time = Time.now.to_i  # or format however you like
-  ext  = File.extname(file)
-  user = %x[echo $USER].chomp
-  File.join(dir, "#{user}_#{base}_#{time}#{ext}")
-end
-
 # set partition or queue to submit a job
 def set_partition(arg)
   partition = ''
@@ -64,16 +53,4 @@ unless ARGV[3].nil?
   exit
 end
 
-script = "#!/bin/bash\n\
-#SBATCH -n 1 # number of cores\n\
-#SBATCH --mail-type=END,FAIL # notifications for job done & fail\n\
-#SBATCH --mail-user=${USER}@nbi.ac.uk # send-to address\n"
-
-temp_script = file_timestamp("/tmp/commands.sh")
-
-%x[echo "#{script}" > #{temp_script}]
-%x[echo "#{cmd}" >> #{temp_script}]
-
-%x[sbatch #{partition} #{memory} #{temp_script}]
-
-%x[rm #{temp_script}]
+%x[sbatch #{partition} #{memory} -n 1 --mail-type=END,FAIL --mail-user=${USER}@nbi.ac.uk --wrap=#{cmd}]
